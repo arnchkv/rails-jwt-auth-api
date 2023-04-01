4dcf3cac85dd97a2202266cc7d0ef71ca8ab025b
Arnav <arnchkv@gmail.com>
2023-04-01 16:01:35 +0530

    jwt added in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,5 +314,12 @@ Devise.setup do |config|
   config.jwt do |jwt|
     # jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/users/sign_in$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/users/sign_out}]
+    ]
+    jwt.expiration_time = 120.minutes.to_i
   end
 end
