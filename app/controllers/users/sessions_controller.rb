5c46b3b5717922a6590dd9a56b0af42bf957ab2a
Arnav <arnchkv@gmail.com>
2023-04-01 17:01:39 +0530

    temp sommit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,30 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  respond_to :json
+
+  private
+
+  def respond_with(resource, option={})
+    render json: {
+      status: { code: 200, message: "User signed in sucessfully", data: current_user }
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload["sub"])
+    if current_user
+      render json: {
+        status: 200,
+        message: "Signed out sucessfully"
+      }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "no acitve session"
+        }, status: :unauthorized
+    end
+  end
 end
